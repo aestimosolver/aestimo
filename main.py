31b52cb5abbdc755ab696574aed4daf6b89e1933
robochat <rjsteed@talk21.com>
2013-04-23 16:45:39 +0200

    removing output files from repository

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ from math import *
 # DO NOT EDIT UNDER THIS POINT
 # ------------------------------------------------------------------------------
 import config
-import aestimo
+import aestimo_numpy
 
 
