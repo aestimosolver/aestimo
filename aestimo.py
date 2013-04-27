c6fb77686f878be58b964a39b7b7ca752abe4305
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2013-04-27 21:38:13 +0100

    Small editing

--- aestimo.py
+++ aestimo.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-  Program:    Aestimo 1D Schrodinger-Poisson Solver
-  Version:    v.0.6
+ Aestimo 1D Schrodinger-Poisson Solver
+ Version v.0.7
+ Copyright (C) 2013 Sefer Bora Lisesivdin and Aestimo group
 
-  Description: This is the aestimo calculator.
-  
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. See ~/COPYING file or http://www.gnu.org/copyleft/gpl.txt .
+
+    For the list of contributors, see ~/AUTHORS
+
+ Description: This is the aestimo calculator. 
 """
 #from scipy.optimize import fsolve
 import matplotlib.pyplot as pl
