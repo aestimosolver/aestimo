d2867cc4c34dfc4dacaf23f23f007e75085c0d33
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2013-04-27 21:39:35 +0100

    New licensing header

--- database.py
+++ database.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-  Program:      Aestimo 1D Schrodinger-Poisson Solver
-  Description:  Database file. Using lists for database entries.
+ Aestimo 1D Schrodinger-Poisson Solver
+ Copyright (C) 2013 Sefer Bora Lisesivdin and Aestimo group
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. See ~/COPYING file or http://www.gnu.org/copyleft/gpl.txt .
+
+    For the list of contributors, see ~/AUTHORS
+
+ Description:  Database file. Using lists for database entries.
                 Absolutely, it is not using a parser subroutine.
                 Quick and dirty solution for the code.
-  References:
+ References:
   - GaAs,AlAs parameters:
     Vurgaftman et al., J. Appl. Phys. 89 (11), 5815 (2001)
   - Si parameters:
