d40fc92b0e062681d4e9d2106ee25d24ea064fdb
robochat <rjsteed@talk21.com>
2013-04-28 19:27:14 +0200

    numpy branch up to date

--- aestimo-numpy.py
+++ aestimo-numpy.py
@@ -29,7 +29,7 @@ meV2J=1e-3*q #meV to Joules
 # Import from config file
 inputfile = __import__(config.inputfilename)
 
-print "Aestimo is starting..."
+print "Aestimo_numpy is starting..."
 
 # Reading inputs and using local variables
 max_val = inputfile.maxgridpoints
@@ -107,6 +107,8 @@ def calc_E_state(numlevels,fi,cb_meff,energyx0): # delta_E,d_E
     E_state=[0.0]*numlevels #Energies of subbands (meV)
     #fi - Potential energy (J)
     #cb_meff - effective mass of electrons in conduction band (kg)
+    fi = fi.tolist() #lists are faster than numpy arrays for loops
+    cb_meff = cb_meff.tolist() #lists are faster than numpy arrays for loops
     for i in range(0,numlevels,1):  
         #increment energy-search for f(x)=0
         y2=psi_at_inf(energyx,fi,cb_meff,n_max,dx)
@@ -137,6 +139,9 @@ def wf(E,fis,cb_meff):
     #	to the energy occurs for psi(+infinity)=0.
     # psi[3] wavefunction at z-delta_z, z and z+delta_z 
     # i index
+    fis = fis.tolist() #lists are faster than numpy arrays for loops
+    cb_meff = cb_meff.tolist() #lists are faster than numpy arrays for loops
+    #
     N = 0.0 # Normalization integral
     psi = []
     psi = [0.0]*3
@@ -157,9 +162,10 @@ def wf(E,fis,cb_meff):
         N += (psi[2])**2
         psi[0]=psi[1]
         psi[1]=psi[2]
-    for j in range(0,n_max,1):
-        b[j]/=(N)**0.5
-    return b # units of dx**0.5
+    b2 = np.array(b)
+    b2/= N**0.5
+    return b2 # units of dx**0.5
+
     
 # FUNCTIONS for FERMI-DIRAC STATISTICS-----------------------------------------   
 def fd2(Ei,Ef,T):
@@ -169,12 +175,8 @@ def fd2(Ei,Ef,T):
 
 def calc_meff_state(wfe,cb_meff):
     #find subband effective mass
-    meff_state = [0.0]*alen(wfe)
-    for j in range(0,subnumber_e,1):
-        total=0.0
-        for b,meff in zip(wfe[j],cb_meff):
-            total+=float(b)**2/meff
-        meff_state[j] = 1.0/total
+    tmp = 1.0/np.sum(wfe**2/cb_meff,axis=1)
+    meff_state = tmp.tolist()
     return meff_state #kg
     
 def fermilevel_0K(Ntotal2d,E_state,meff_state):
@@ -225,7 +227,7 @@ def calc_N_state(Ef,T,Ns,E_state,meff_state):
     
 # FUNCTIONS for SELF-CONSISTENT POISSON--------------------------------
 def dop0():
-    dop = [0.0]*n_max
+    dop = np.zeros(n_max)
     for layer in material:
         startindex = int(layer[1]/dx)
         finishindex = int(layer[2]/dx)
@@ -233,8 +235,8 @@ def dop0():
             chargedensity = -layer[5]*1e6 #charge density in m**-3 (conversion from cm**-3)
         elif layer[6] == 'p': 
             chargedensity = layer[5]*1e6 #charge density in m**-3 (conversion from cm**-3)
-        for j in range(startindex,finishindex):
-            dop[j] = chargedensity
+        
+        dop[startindex:finishindex] = chargedensity
     return dop
 
 
@@ -242,15 +244,12 @@ def calc_sigma(wfe,N_state,dop):
     # This function calculates `net' areal charge density
     # i index over z co-ordinates
     # is index over states
-    sigma = [0.0]*n_max
-    for i in range(0,n_max,1):
-        for j in range(0,subnumber_e,1):
-            sigma[i] = sigma[i] - N_state[j]*(float(wfe[j][i])**2)
-            # n-type dopants give -ve *(N+j) representing electrons, hence 
-            # addition of +ve ionised donors requires -*(Nda+i), note Nda is still a
-            # volume density, the delta_z converts it to an areal density
-        sigma[i] = sigma[i] - dop[i]*dx # This may be one tab indented.
-
+    sigma= -dop*dx # This may be one tab indented.
+    for j in range(0,subnumber_e,1):
+        sigma-= N_state[j]*(wfe[j])**2
+        # n-type dopants give -ve *(N+j) representing electrons, hence 
+        # addition of +ve ionised donors requires -*(Nda+i), note Nda is still a
+        # volume density, the delta_z converts it to an areal density
     return sigma
     
 ##
@@ -258,19 +257,21 @@ def calc_field(sigma,eps):
     # F electric field as a function of z-
     # i index over z co-ordinates
     # j index over z' co-ordinates
-
-    # For wave function initialise F
-    #for i in range(0,n_max,1): #It isn't really necessary to zero everything when using the running integral form.
-    #    F[i] = 0.0
-    F = [0.0]*n_max
-    # Do zeroth case explicitly - in fact, normally we can assume that the total electric field is zero (?)
-    for j in range(1,n_max,1):
-        # Note sigma is a number density per unit area, needs to be converted to Couloumb per unit area
-        F[0] -= q*sigma[j]/(2.0*eps[0]) #CMP'deki i ve j yer değişebilir - de + olabilir
+    # Note: sigma is a number density per unit area, needs to be converted to Couloumb per unit area
+    F0 = -np.sum(q*sigma)/(2.0) #CMP'deki i ve j yer değişebilir - de + olabilir
+    # is the above necessary since the total field due to the structure should be zero.
     # Do running integral
-    for i in range(1,n_max,1):
-        # Note sigma is a number density per unit area, needs to be converted to Couloumb per unit area
-        F[i] = F[i-1]*(eps[i-1]/eps[i]) + q*(sigma[i-1]+sigma[i])/(2.0*eps[i]) #CMP'deki i ve j yer değişebilir - de + olabilir
+    tmp = np.hstack(([0.0],sigma[:-1])) + sigma
+    tmp*= q/2.0 # Note: sigma is a number density per unit area, needs to be converted to Couloumb per unit area
+    tmp[0] = F0 
+    F = np.cumsum(tmp)/eps
+    return F
+
+def calc_field_convolve(sigma,eps):
+    tmp = np.ones(n_max-1)
+    signstep = np.hstack((-tmp,[0.0],tmp)) # step function
+    F = np.convolve(signstep,sigma,mode='valid')
+    F*= q/(2.0*eps)
     return F
 
 def calc_field_old(sigma,eps):
@@ -279,23 +280,21 @@ def calc_field_old(sigma,eps):
     # j index over z' co-ordinates
 
     # For wave function initialise F
-    for i in range(0,n_max,1):
-        F[i] = 0.0
+    F[:] = 0.0
     for i in range(0,n_max,1):
         for j in range(0,n_max,1):
-            # Note sigma is a number density per unit area, needs to be converted to Couloumb per unit area
-            F[i] = F[i] + q*sigma[j]*cmp(i,j)/(2*eps[i]) #CMP'deki i ve j yer değişebilir - de + olabilir
+           # Note sigma is a number density per unit area, needs to be converted to Couloumb per unit area
+           F[i] = F[i] + q*sigma[j]*cmp(i,j)/(2*eps[i]) #CMP'deki i ve j yer değişebilir - de + olabilir
     return F
 
 def calc_potn(F):
     # This function calculates the potential (energy actually)
     # V electric field as a function of z-
     # i	index over z co-ordinates
-    
+
     #Calculate the potential, defining the first point as zero
-    V = [0.0] * n_max
-    for i in range(1,n_max,1):
-        V[i]=V[i-1]+q*F[i]*dx #+q -> electron -q->hole? 
+    tmp = q*F*dx
+    V = np.cumsum(tmp) #+q -> electron -q->hole? 
     return V
 
 
@@ -311,37 +310,38 @@ def fill_structure_lists():
         
         if matType in material_property:
             matprops = material_property[matType]
-            for i in range(startindex,finishindex):
-                cb_meff[i] = matprops[0]*m_e
-                fi[i] = matprops[4]*matprops[3]*q #Joule
-                eps[i] = matprops[2]*eps0
+            cb_meff[startindex:finishindex] = matprops[0]*m_e
+            fi[startindex:finishindex] = matprops[4]*matprops[3]*q #Joule
+            eps[startindex:finishindex] = matprops[2]*eps0
             
         elif matType in alloy_property:
-            alloyprops = alloy_property[matType]
-            for i in range(startindex,finishindex):            
-                cb_meff[i] = (alloyprops[0]+alloyprops[1]*layer[4])*m_e
-                fi[i] = alloyprops[4]*layer[4]*q*alloyprops[5] # for electron. Joule
-                eps[i] = (alloyprops[2]+alloyprops[3]*layer[4])*eps0
+            alloyprops = alloy_property[matType]            
+            cb_meff[startindex:finishindex] = (alloyprops[0]+alloyprops[1]*layer[4])*m_e
+            fi[startindex:finishindex] = alloyprops[4]*layer[4]*q*alloyprops[5] # for electron. Joule
+            eps[startindex:finishindex] = (alloyprops[2]+alloyprops[3]*layer[4])*eps0
  
 # ----------------------------------------------------
+
 
 # Preparing empty subband energy lists.
 E_state = [0.0]*subnumber_e     # Energies of subbands/levels (meV)
 N_state = [0.0]*subnumber_e     # Number of carriers in subbands  
 
 # Creating and Filling material arrays
-cb_meff = [0.0]*n_max	#conduction band effective mass
-fi = [0.0]*n_max	#Bandstructure potential
-fitot = [0.0]*n_max	#Energy potential = Bandstructure + Coulombic potential
-eps =[0.0]*n_max	#dielectric constant
-dop = [0.0]*n_max	#doping distribution
-sigma = [0.0]*n_max	#charge distribution (donors + free charges)
-F = [0.0]*n_max		#Electric Field
-V = [0.0]*n_max		#Electric Potential
-Vapp = [0.0]*n_max	#Electric Potential
+xaxis = np.arange(0,n_max)*dx   #metres
+cb_meff = np.zeros(n_max)	#conduction band effective mass
+fi = np.zeros(n_max)		#Bandstructure potential
+fitot = np.zeros(n_max)		#Energy potential = Bandstructure + Coulombic potential
+eps =np.zeros(n_max)		#dielectric constant
+dop = np.zeros(n_max)		#doping distribution
+sigma = np.zeros(n_max)         #charge distribution (donors + free charges)
+F = np.zeros(n_max)		#Electric Field
+V = np.zeros(n_max)             #Electric Potential
+Vapp = np.zeros(n_max)			#Applied Electric Potential
+
 
 # Subband wavefunction for electron list. 2-dimensional: [i][j] i:stateno, j:wavefunc
-wfe = np.zeros((subnumber_e,n_max),dtype = float)
+wfe = np.zeros((subnumber_e,n_max))
 
 # Initialise Arrays
 fill_structure_lists()
@@ -366,29 +366,21 @@ if abs(E_start)<1e-3*meV2J: #energyx is the minimum energy (meV) when starting t
     energyx = fi_min
 else:
     energyx = E_start
-    
-# Applied Field
-x0=x_max/2.0 # Finding the middle point (z0) of z-axis for Fapp
-for i in range(0,n_max,1):
-    Vapp[i] = q*Fapp*(i*dx-x0)
+
+#Applied Field
+x0 = x_max/2.0
+Vapp = q*Fapp*(xaxis-x0)
 
 # STARTING SELF CONSISTENT LOOP
 iteration = 1   #iteration counter
 previousE0= 0   #(meV) energy of zeroth state for previous iteration(for testing convergence)
-#fitot = list(fi) #For initial iteration just copy fi. list(seq) returns a copy of the original rather than just an alias.
-for i in range(0,n_max,1):
-    fitot[i] = fi[i] + Vapp[i]  # Adding field qF(z-z0)
+fitot = fi + Vapp #For initial iteration sum bandstructure and applied field
 
 while True:
     if not(config.messagesoff) :
         print "Iteration:",iteration
     if iteration> 1:
-        for i in range(0, n_max, 1):
-            # Find fi-minimum --may got error.
-            if fitot[i] < fi_min:
-                energyx = fitot[i]
-            else:
-                energyx = fi_min
+        energyx=min(fi_min,min(fitot),)
     
     E_state=calc_E_state(subnumber_e,fitot,cb_meff,energyx)
     
@@ -397,7 +389,7 @@ while True:
         if not(config.messagesoff) :
             print "Working for subband no:",j+1
         wfe[j] = wf(E_state[j]*meV2J,fitot,cb_meff) #wavefunction units dx**0.5
-    
+
     # Calculate the effective mass of each subband
     meff_state = calc_meff_state(wfe,cb_meff)
     
@@ -418,10 +410,10 @@ while True:
     # Combine band edge potential with potential due to charge distribution
     # To increase convergence, we calculate a moving average of electric potential 
     #with previous iterations. By dampening the corrective term, we avoid oscillations.
-    for i in range(0,n_max,1):
-        V[i] = V[i] + damping*(Vnew[i] - V[i])
-        fitot[i] = fi[i] + V[i] + Vapp[i]
-        
+
+    V+= damping*(Vnew - V)
+    fitot = fi + V + Vapp
+    
     #status
     if not(config.messagesoff):
         for i,level in enumerate(E_state):
@@ -434,9 +426,9 @@ while True:
         #for i,Ni in enumerate(N_state_0K):
         #    print 'N[',i,']= ',Ni
         print 'Efermi (at %gK) = ' %T, E_F,' meV'
-        print "total donor charge = ",sum(dop)*dx,"m**-2"
+        print "total donor charge = ",np.sum(dop)*dx,"m**-2"
         print "total level charge = ",sum(N_state),"m**-2"
-        print "total system charge = ",sum(sigma),"m**-2"
+        print "total system charge = ",np.sum(sigma),"m**-2"
     
     if abs(E_state[0]-previousE0) < convergence_test: #Convergence test
         break
@@ -451,21 +443,21 @@ while True:
 
 # Write the simulation results in files
 
-xaxis = np.arange(0,n_max)*dx   #metres
+#xaxis = np.arange(0,n_max)*dx   #metres
 
 def saveoutput(fname,datatuple):
     np.savetxt(fname,np.column_stack(datatuple),fmt='%.6e', delimiter=' ')
     
 if config.sigma_out:
-    saveoutput("outputs/sigma.dat",(xaxis,sigma))
+    saveoutput("outputs-numpy/sigma.dat",(xaxis,sigma))
 if config.electricfield_out:
-    saveoutput("outputs/efield.dat",(xaxis,F))
+    saveoutput("outputs-numpy/efield.dat",(xaxis,F))
 if config.potential_out:
-    saveoutput("outputs/potn.dat",(xaxis,fitot))
+    saveoutput("outputs-numpy/potn.dat",(xaxis,fitot))
 if config.states_out:
-    saveoutput("outputs/states.dat",(range(subnumber_e),N_state,E_state,meff_state) )
+    saveoutput("outputs-numpy/states.dat",(range(subnumber_e),N_state,E_state,meff_state) )
 if config.probability_out:
-    saveoutput("outputs/wavefunctions.dat",(xaxis,wfe.transpose()) )
+    saveoutput("outputs-numpy/wavefunctions.dat",(xaxis,wfe.transpose()) )
 
 # Resultviewer
     
@@ -516,11 +508,11 @@ if config.resultviewer:
     pl.figure(figsize=(10,8))
     pl.suptitle('Aestimo Results')
     pl.subplot(1,1,1)
-    pl.plot(xaxis,np.array(fitot)*J2meV,'k')
+    pl.plot(xaxis,fitot*J2meV,'k')
     for level,state in zip(E_state,wfe): 
         pl.axhline(level,0.1,0.9,color='g',ls='--')
-        pl.plot(xaxis, np.array(state)*200.0+level,'b')
-        #pl.plot(xaxis, np.array(state)**2*1e-9/dx*200.0+level,'b')
+        pl.plot(xaxis, state*200.0+level,'b')
+        #pl.plot(xaxis, state**2*1e-9/dx*200.0+level,'b')
     pl.axhline(E_F,0.1,0.9,color='r',ls='--')
     pl.xlabel('Position (m)')
     pl.ylabel('Energy (meV)')
